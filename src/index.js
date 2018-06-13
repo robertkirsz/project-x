aa5de37f201081d7c2fd007b1e004aa18a06d9d3
Robert Kirsz <robert.kirsz@gmail.com>
2018-06-13 21:51:02 +0200

    Add Material theme

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 import 'styles'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#F39100',
+      main: '#F39100',
+      dark: '#F39100'
+    }
+  }
+})
+
 ReactDOM.render(
   <Fragment>
-    <Router>
-      <Route path="/" component={App} />
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
+    </MuiThemeProvider>
     <CssBaseline />
   </Fragment>,
   document.getElementById('root')

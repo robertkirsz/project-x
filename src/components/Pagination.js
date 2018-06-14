bad922688e8f5457349c27ff5f187f55f403ebb0
Robert Kirsz <robert.kirsz@gmail.com>
2018-06-14 20:08:09 +0200

    Add Pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Div } from 'styled-kit'
+
+export default ({ size, value, onChange, ...props }) => (
+  <Div flex="none" itemsCenter listLeft={12} {...props}>
+    {[...Array(size)].map((element, index) => (
+      <Dot key={index} isActive={value === index} onClick={() => onChange(index)} />
+    ))}
+  </Div>
+)
+
+const Dot = styled.span`
+  display: block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background: ${props => (props.isActive ? 'black' : 'rgba(0, 0, 0, 0.2)')};
+  transition: 0.3s;
+  cursor: pointer;
+`

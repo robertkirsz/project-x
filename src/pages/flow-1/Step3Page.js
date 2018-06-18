d6f50c02cb4f219b9037f2f5e595cdd6a48fdce9
Robert Kirsz <robert.kirsz@gmail.com>
2018-06-18 21:35:55 +0200

    Add base for step 3

---
+++ src/pages/flow-1/Step3Page.js
@@ -0,0 +1,78 @@
+import React, { Component, Fragment } from 'react'
+import { Div } from 'styled-kit'
+import { Route } from 'react-router-dom'
+
+// import TextField from '@material-ui/core/TextField'
+// import InputLabel from '@material-ui/core/InputLabel'
+// import MenuItem from '@material-ui/core/MenuItem'
+// import FormControl from '@material-ui/core/FormControl'
+// import Select from '@material-ui/core/Select'
+// import FormControlLabel from '@material-ui/core/FormControlLabel'
+// import Checkbox from '@material-ui/core/Checkbox'
+// import Radio from '@material-ui/core/Radio'
+// import RadioGroup from '@material-ui/core/RadioGroup'
+
+import { H1 } from 'components/Typography'
+// import { H1, H2, Paragraph, Small, Link } from 'components/Typography'
+import StepStatus, { Step } from 'components/StepStatus'
+import Button from 'components/Button'
+import Progress from 'components/Progress'
+
+import logo from 'assets/logo.svg'
+
+const paths = [
+  '/onboarding-1/step-3/pin-setup',
+  '/onboarding-1/step-3/pin-confirm',
+  '/onboarding-1/step-3/password-setup',
+  '/onboarding-1/step-3/email-confirm'
+]
+
+export default class Step3Page extends Component {
+  state = {}
+
+  change = name => value => this.setState({ [name]: value })
+
+  handleChange = name => event => this.setState({ [name]: event.target.value })
+
+  handleCheckboxChange = name => event => this.setState({ [name]: event.target.checked })
+
+  isValid = keys => {
+    for (let index in keys) return Boolean(this.state[keys[index]])
+  }
+
+  render() {
+    const currentStep = paths.findIndex(path => path === this.props.location.pathname)
+
+    const intro = (
+      <Div flex={1} column itemsCenter padding="30px 16px">
+        <img src={logo} alt="Logo" width="108" />
+
+        <H1 mTop={13}>Get your new mBank account in only 3 simple steps and start with...</H1>
+
+        <StepStatus>
+          <Step number="1" isActive>
+            Personal data
+          </Step>
+          <Step number="2">Video identification</Step>
+          <Step number="3">PIN & password setup</Step>
+        </StepStatus>
+
+        <Button onClick={() => this.props.history.push('/onboarding-1/step-3/name')} style={{ marginTop: 'auto' }}>
+          Next step
+        </Button>
+      </Div>
+    )
+
+    return (
+      <Fragment>
+        {!this.props.match.isExact && (
+          <Progress currentStep={currentStep} paths={paths}>
+            Lorem ipsum
+          </Progress>
+        )}
+
+        <Route path="/onboarding-1/step-3" exact render={() => intro} />
+      </Fragment>
+    )
+  }
+}

19dbef29a04d886dc7606b54b0115738d2739aea
Robert Kirsz <robert.kirsz@gmail.com>
2018-06-19 19:36:01 +0200

    Add remaining assets

--- src/utils/preloadImages.js
+++ src/utils/preloadImages.js
@@ -12,6 +12,12 @@ import usp1 from 'assets/usp-1.png'
 import usp2 from 'assets/usp-2.png'
 import usp3 from 'assets/usp-3.png'
 import usp4 from 'assets/usp-4.png'
+import video1 from 'assets/video-identification-1.svg'
+import video2 from 'assets/video-identification-2.svg'
+import video3 from 'assets/video-identification-3.svg'
+import video4 from 'assets/video-identification-4.svg'
+import pinLogo from 'assets/pin-logo.svg'
+import mail from 'assets/mail.svg'
 
 const images = [
   arrowLeft,
@@ -27,18 +33,23 @@ const images = [
   usp1,
   usp2,
   usp3,
-  usp4
+  usp4,
+  video1,
+  video2,
+  video3,
+  video4,
+  pinLogo,
+  mail
 ]
 
-export default () => {
-  const promises = images.map(
-    url =>
-      new Promise(resolve => {
-        const img = new Image()
-        img.src = url
-        img.onload = () => resolve()
-      })
+export default () =>
+  Promise.all(
+    images.map(
+      src =>
+        new Promise(resolve => {
+          const img = new Image()
+          img.src = src
+          img.onload = () => resolve()
+        })
+    )
   )
-
-  return Promise.all(promises)
-}
